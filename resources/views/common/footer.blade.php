fe9a47afccb40d025ee535c3aa83c5402d2006f5
Koreyoshi159 <70514758+Koreyoshi159@users.noreply.github.com>
2022-01-18 16:49:43 +0100

    Update footer.blade.php

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="content has-text-centered">
         <p>
-        <strong>V1.1.0</strong> 
+        <strong>V1.1.1</strong> 
         </p> 
     </div>
 </footer>
